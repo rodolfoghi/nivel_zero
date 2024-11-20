28dc4b0c6a3ea87cffce57fb12805b4d6af8c6f4
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2024-11-20 08:30:33 -0300

    Config pt-BR locale

--- app/models/ncm.rb
+++ app/models/ncm.rb
@@ -1,2 +1,8 @@
 class Ncm < ApplicationRecord
+  include ActiveModel::Attributes
+
+  validates :codigo, presence: true, length: { minimum: 8, maximum: 8 }
+  validates :descricao, presence: true, length: {  maximum: 120 }
+
+  attribute :ativa, :boolean, default: true
 end
